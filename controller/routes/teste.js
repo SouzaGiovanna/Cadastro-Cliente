eef39e880a0eaa309d64a729d0bfd4b82c454069
SouzaGiovanna <gi.souza03@gmail.com>
2022-03-16 09:37:36 -0300

    vamo testa

---
+++ controller/routes/teste.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+const app = express();
+
+app.get("/", function(req, resp){
+    resp.send("Bem-Vindo ao meu app");
+});
+
+app.get("/contato", function(req, resp){
+    resp.send("Página de contato do meu app");
+});
+
+app.get("/produto", function(req, resp){
+    resp.send("Página de produto do meu app");
+})
+
+// esta sempre deve ser a ilyima linha quando usamos o express
+app.listen(8081, function(){
+    console.log("Servidor funcionando na url http://localhost:8081");
+});
